fcb329fe989fdb65c88669fd92209900ec27a47b
NickBanken <nick.banken@gmail.com>
2022-12-16 12:28:37 +0100

    creation of seperate list component

---
+++ src/components/ui/AllListComponent.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { storeToRefs } from "pinia";
+
+import CardComponentVue from "@/components/home/CardComponent.vue";
+
+const { pokemons } = defineProps(["pokemons"]);
+</script>
+
+<template>
+  <transition
+    name="fade"
+    v-if="pokemons.length > 0"
+    class="flex flex-col"
+    tag="ul"
+    appear
+  >
+    <transition-group name="list">
+      <card-component-vue
+        :key="pokemon.name"
+        :pokemon="pokemon"
+        v-for="pokemon in pokemons"
+      />
+    </transition-group>
+  </transition>
+  <h2 class="mt-20 text-xl text-center" v-else>
+    Sorry, we could not find any Pokemon with that value.
+  </h2>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.list-move,
+.list-enter-to
+ /* apply transition to moving elements */
+.list-enter-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter,
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  /* transform: translateX(30px); */
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  opacity: 0;
+  position: absolute;
+}
+</style>
+
+<!-- <style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style> -->
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,25 +1,27 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import type { Ref } from "vue";
-import { usePokemonStore } from "@/stores/PokemonStore";
-import { storeToRefs } from "pinia";
+import { onBeforeMount, computed, defineProps, ref } from "vue";
 
-import { onBeforeMount } from "vue";
+import AllListComponent from "@/components/ui/AllListComponent.vue";
+import Navigation from "@/components/ui/Navigation.vue";
+import { usePokemonStore } from "@/stores/PokemonStore";
+
 import type singlePokemon from "@/types/singlePokemon";
+import type { Ref } from "vue";
 
 const pokemonStore = usePokemonStore();
-const { localKeyFavourite } = storeToRefs(pokemonStore);
-let listedPokemon: Ref<singlePokemon[]> = ref([]);
 
-onBeforeMount(async () => {
-  listedPokemon = pokemonStore.getLocalStorage(localKeyFavourite.value);
+let listedPokemon: Ref<singlePokemon[]> = ref([]);
+let apikey: Ref<String | undefined> = ref<String | undefined>();
+
+onBeforeMount(() => {
+  listedPokemon = pokemonStore.getLocalStorage("favourite-pokedex-wisemen");
 });
 </script>
 
 <template>
-  <ul v-for="pokemon in listedPokemon">
-    {{
-      pokemon.name
-    }}
-  </ul>
+  <main class="flex h-screen flex-col bg-[#15D0DC] px-5">
+    <Navigation />
+    <h2 class="mb-6 text-[34px] font-bold text-white">Favorieten</h2>
+    <AllListComponent :pokemons="listedPokemon"></AllListComponent>
+  </main>
 </template>
